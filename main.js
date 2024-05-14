19c276abce21613fbd9735f0ee8bbff8a55bb804
Carlos Augusto Aquino <carlos.aquino@aluno.uece.br>
2024-05-14 20:46:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play()
 };
 
-document.querySelector('#som_tecla_pom');
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
